fb0f991a9fd68657808d295d00a89de3747056fb
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-06-25 23:42:32 -0300

    array

--- numpy/numpy1.py
+++ numpy/numpy1.py
@@ -1,5 +1,22 @@
 import numpy as np
+import time
+array = np.array([1, 2 ,3])
+novo_array = array * 10
 
-a = np.array(1, 2 ,3)
+print(novo_array)
 
-print(a)
+
+lista = list(range(1, 1000000))
+listanp = np.array(range(1, 1000000))
+
+
+inicio = time.time()
+lista_soma = sum(lista)
+fim = time.time()
+print(f'TEMPO NESCESSARIO lista normal: { fim - inicio }')
+
+inicio = time.time()
+lista_soma = np.sum(listanp)
+fim = time.time()
+print(f'TEMPO NESCESSARIO array np: { fim - inicio }')
+
