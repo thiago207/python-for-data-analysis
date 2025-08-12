662998d87b7119e379bd4fb9b67a5ebd85b6b9f7
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-11 21:14:58 -0300

    Create aula1.py

---
+++ streamlit/aula1.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import yfinance  as yf
+
+
+
+
+
+
+
+
+# = tamanho do titulo
+st.write("""
+# App de preço de Ações
+O gráfic o apresenta a evolução do preço das ações do Itau (ITUB4) ao longo dos anos
+""")
