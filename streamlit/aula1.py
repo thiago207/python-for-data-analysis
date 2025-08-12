23c43b2e08aaf06996b4b8c0fe72459b45b6ecc9
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-11 21:40:58 -0300

    Update aula1.py

--- streamlit/aula1.py
+++ streamlit/aula1.py
@@ -3,14 +3,25 @@ import pandas as pd
 import yfinance  as yf
 
 
+@st.cache_data
+def carregar_empresa(empresa):
+    dados_acao = yf.Ticker(empresa)
+    cotacao_acao = dados_acao.history(start='2000-01-01', end='2024-07-01')
+    cotacao_acao = cotacao_acao['Close']
+    return cotacao_acao
 
 
 
-
+dados = carregar_empresa("ITUB4.SA")
 
 
 # = tamanho do titulo
 st.write("""
-# App de preço de Ações
-O gráfic o apresenta a evolução do preço das ações do Itau (ITUB4) ao longo dos anos
+# App de preço de Ações  
+O gráfico apresenta a evolução do preço das ações brasileiras do Itau (ITUB4) ao longo dos anos
 """)
+
+st.line_chart(dados)
+
+
+st.write(dados)
