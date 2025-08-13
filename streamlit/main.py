f5cdd79643b27d8082234d55d29609e71c1aa8ea
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-12 21:54:37 -0300

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -41,7 +41,9 @@ tickers_selecionados = st.sidebar.multiselect(
 
 data_inicial = dados.index.min().to_pydatetime()
 data_final = dados.index.max().to_pydatetime()
-st.sidebar.slider('Selecione o periodo', min_value=data_inicial, max_value=data_final)
+intervalo_data = st.sidebar.slider('Selecione o periodo', min_value=data_inicial, max_value=data_final, value=(data_inicial, data_final))
+
+dados = dados.loc[intervalo_data[0], intervalo_data[1]]
 
 # Gráfico filtrado
 if tickers_selecionados:
